4eebd486131e287ec42410e0eebc557c7c00ea18
Fudack <felipe.medina33@inacapmail.cl>
2023-01-11 13:14:49 -0300

    aperture of camera in 90% and view image in 80%

--- app/src/main/java/com/example/visionartificial/MainActivity.java
+++ app/src/main/java/com/example/visionartificial/MainActivity.java
@@ -1,13 +1,40 @@
 package com.example.visionartificial;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btncamara;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btncamara = findViewById(R.id.btncamara);
+        imageView = findViewById(R.id.imageView);
+
+        btncamara.setOnClickListener(view -> abrirCamara());
+    }
+
+    private void abrirCamara(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivityForResult(intent, 1);
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(imgBitmap);
+        }
     }
 }
