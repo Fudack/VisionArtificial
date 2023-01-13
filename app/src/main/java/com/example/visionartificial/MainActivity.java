bb466b73a57b38b37ca9ae6984a140f2ca19fa15
Fudack <felipe.medina33@inacapmail.cl>
2023-01-13 12:46:40 -0300

    implementation androidx for compatibility api cameraX

--- app/src/main/java/com/example/visionartificial/MainActivity.java
+++ app/src/main/java/com/example/visionartificial/MainActivity.java
@@ -4,12 +4,13 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,16 +31,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void abrirCamara(){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if(intent.resolveActivity(getPackageManager()) != null){
-            startActivityForResult(intent, 1);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            super.startActivityForResult(intent,1);
         } else {
             Toast.makeText(this, "", Toast.LENGTH_SHORT).show();
         }
 
     }
 
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
-        if (requestCode == 1 && resultCode == RESULT_OK){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
             Bundle extras = data.getExtras();
             Bitmap imgBitmap = (Bitmap) extras.get("data");
             imagePreview.setImageBitmap(imgBitmap);
