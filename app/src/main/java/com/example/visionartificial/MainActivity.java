34850f082b4b7daba4dfd4d40d4b6aed0d412605
RodrigoMardonesAlvarez <rodrigomardones@protonmail.com>
2023-01-21 15:23:25 -0300

    Clear documentation comments

--- app/src/main/java/com/example/visionartificial/MainActivity.java
+++ app/src/main/java/com/example/visionartificial/MainActivity.java
@@ -120,13 +120,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
 
             if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
                 return;
             }
             videoCapture.startRecording(
